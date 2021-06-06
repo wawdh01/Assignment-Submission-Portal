348896c3ec1bc5328e29629eddba1a3aaa96b0c3
Gaurav Wawdhane <gaurav.wawdhane@gmail.com>
2021-06-06 17:57:55 +0530

    get / added

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/', (req, res)=>{
+	res.render('index', {});
+})
+
 
 app.listen(PORT, ()=>console.log(`The server is started at PORT : ${PORT}`));
 
